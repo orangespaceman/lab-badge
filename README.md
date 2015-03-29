ca5e76da23fd2c8d90a58cee3c07ef78fb574c74
Pete Goodman <thegingerbloke@users.noreply.github.com>
2015-03-29 20:43:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# lab-badge
+
+Add a lab badge to any website by including a single JavaScript file.
+
+
+## Usage
+
+Just save the `lab-badge.min.js` file from this repository into the project, and add the following line of JavaScript to the site:
+
+    <script src="path/to/lab-badge.min.js"></script>
+    
+E.G.
+
+	<script src="http://thegingerbloke.github.io/lab-badge/lab-badge.min.js"></script>
+
+That's it. The script will automatically initialise when the page has finished loading.
+
+
+## Example
+
+This repo contains an example HTML page that shows how to use the script.
+
+
+## Source
+
+This repo also contains the source code. If a change is made, the minified file needs to be manually recompiled.
+
+This can be done with an [online compressor](http://refresh-sf.com/)
+
+
+## Deploy
+
+To publish `lab-badge.min.js` to Github pages (e.g. [http://thegingerbloke.github.io/lab-badge/lab-badge.min.js](http://thegingerbloke.github.io/lab-badge/lab-badge.min.js)), the file `lab-badge.min.js` should be copied into the root of the `gh-pages` branch
---
+++ lab-badge.js
@@ -0,0 +1,78 @@
+/*
+ * Add a lab badge to a site
+ */
+(function(){
+
+    var linkText = "Back to my website";
+    var linkHref = "http://petegoodman.com";
+    var paraId = "lab-badge";
+    var css = "#lab-badge { position:fixed !important; left: auto !important; right:0 !important; top:auto !important; bottom:100px !important; padding:0 !important; margin:0 !important; background:none !important; }" +
+            "#lab-badge a { display:block !important; position:relative !important; right: -112px !important; top:0 !important; background: rgba(255, 153, 0, 0.5) !important; border:3px solid #f90 !important; border-top-left-radius:30px !important; border-bottom-left-radius:30px !important; border-right:none !important; padding:8px 8px 8px 40px !important; color:#fff !important; font-family:Georgia, sans-serif !important; font-size:12px !important; line-height:1.5 !important; font-style:italic !important; font-weight:normal !important; text-decoration:none !important; transition:all 0.25s ease-in-out; }" +
+            "#lab-badge span { display:block !important; position: absolute !important; left: 8px !important; top: 5px !important; color: #fff !important; font-size: 20px !important; line-height: 1 !important; letter-spacing: 1px !important; }" +
+            "#lab-badge a:hover { right:0 !important; color:#fff !important; border-color:#f90 !important; background: rgba(255, 153, 0, 0.75) !important;  }";
+
+    // add CSS
+    function addCss () {
+        var s = document.createElement("style");
+        s.type = "text/css";
+        if (s.styleSheet) {
+            s.styleSheet.cssText = css;
+        } else {
+            s.appendChild(document.createTextNode(css));
+        }
+        document.getElementsByTagName("head")[0].appendChild(s);
+    }
+
+    // init
+    function init() {
+
+        // elements
+        var body, p, a, span,
+            d = document;
+
+        body = d.getElementsByTagName('body')[0];
+
+        // create the paragraph container as a child of the BODY element
+        p = body.appendChild(d.createElement("p"));
+        p.id = paraId;
+
+
+        // create an anchor element for link
+        a = d.createElement("a");
+        a.innerHTML = linkText;
+        a.href = linkHref;
+        p.appendChild(a);
+
+        // create an anchor element for link
+        span = d.createElement("span");
+        span.innerHTML = "<>";
+        a.appendChild(span);
+
+        addCss();
+
+        // set up the onclick event to open new window
+        addEvent(a, 'click', function(e) {
+            window.open(linkHref);
+            if (e) {
+                e.preventDefault();
+            }
+            return false;
+        });
+    }
+
+    // add event - x browser
+    function addEvent(obj, evType, fn){
+        if (obj.addEventListener){
+            obj.addEventListener(evType, fn, false);
+            return true;
+        } else if (obj.attachEvent){
+            var r = obj.attachEvent("on"+evType, fn);
+            return r;
+        } else {
+            return false;
+        }
+    }
+
+    // init
+    addEvent(window, 'load', init);
+})();
